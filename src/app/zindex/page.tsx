eacbd60fa9e606ef600f0a9c0380fc1b7f670463
abeyuhang <abeyuhang@gmail.com>
2023-10-25 21:31:07 +0800

    update comment

--- src/app/zindex/page.tsx
+++ src/app/zindex/page.tsx
@@ -1,5 +1,13 @@
 'use client';
 
+/**
+ * Modal 和 Popup 相互嵌套递归，可以无限进行弹窗再弹窗，新弹出层的蒙版应该遮挡好旧的层。
+ * Modal 和 Popup 中都有 Tooltip 和 Toast（Message）。
+ * Tooltip 需要在当前弹窗层的内容之上展示，但能够被下一层新的弹窗遮挡，无限递归下去后每一层的 Tooltip 都满足这个要求。
+ * Toast 一般是比如操作提交 api 接口后的成功或失败提示，需要永远在最顶上，不论当前有多少层弹窗。
+ * 这个示例以及使用到的相关组件，达到上述目标，不是靠不同组件约定某个特定的 zIndex，而是统一的进行动态化 z-index 管理。
+ */
+
 import { FC, useState } from 'react';
 import { Button } from 'preline-react';
 import { Modal } from 'preline-react';
